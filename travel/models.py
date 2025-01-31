d9b684a01ba8d1a6b7a9e095a1e6552a153f8c1a
Jamil <jamil.ashfaq04@gmail.com>
2025-01-31 12:34:06 +0600

    fix: user model

--- travel/models.py
+++ travel/models.py
@@ -6,7 +6,20 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='travel_users',  # Changed from user_set
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='travel_users',  # Changed from user_set
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
 class District(models.Model):
     name = models.CharField(max_length=100)
