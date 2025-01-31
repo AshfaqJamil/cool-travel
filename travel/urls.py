988f956606a59631c12202a469fa1144c28f85d1
Jamil <jamil.ashfaq04@gmail.com>
2025-01-31 12:51:06 +0600

    add: register api endpoint

--- travel/urls.py
+++ travel/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CoolestDistrictsAPIView, TravelDecisionAPIView
+from .views import CoolestDistrictsAPIView, TravelDecisionAPIView, RegisterView
 from .views import LoginView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('coolest-districts/', CoolestDistrictsAPIView.as_view(), name='coolest_districts'),
     path('travel-decision/', TravelDecisionAPIView.as_view(), name='travel_decision'),
